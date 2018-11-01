348361cac23ad5f317826305108c626aafa1d584
Purity <ronopurity05@gmail.com>
2018-11-02 01:56:42 +0300

    success

--- success.php
+++ success.php
@@ -96,6 +96,9 @@ include 'session.php';
     ?>
 
 </div>
+<div class="checkout-right-basket">
+    <a href="posts.php"><span class="glyphicon glyphicon-menu-left" aria-hidden="true"></span>Continue Shopping</a>
+</div>
 
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
